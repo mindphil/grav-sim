9c4fb97909c9650bcb77d3ec8bc99a4fef006d3d
mindphil <philipumeadi@icloud.com>
2025-07-28 21:44:11 -0400

    learning the ropes

---
+++ math.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+// Initializing integers in one way
+
+int main(){
+    
+    // Initializing integers in one way
+
+    int a, b, c;
+    a = 5, b = 3;
+    c = a + b;
+    
+    // Initializing integers in another way
+    
+    int d(3), e(2), f(a - b);
+
+    // Initializing integers in a third way
+
+    auto g{10};
+    auto h{5};
+    auto i{g / h};
+    
+    // output result in terminal with cout
+
+    cout << "c: " << c << endl << "f: " << f << endl << "i: " << i << endl;
+
+    // Output result in terminal with printf
+
+    printf ("c: %d\n", c), printf("f: %d\n", f), printf("i: %d\n", i);
+
+    // end program
+
+    return 0;
+}
