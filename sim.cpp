61978c5db81a2a9f9f499fc6b3491c51a7d27702
mindphil <philipumeadi@icloud.com>
2025-07-28 22:49:59 -0400

    defining gravity for any mass

--- sim.cpp
+++ sim.cpp
@@ -2,3 +2,19 @@
 #include <string>
 
 using namespace std;
+
+// Defining the gravity for any mass
+
+int main()
+{
+    float g, m, a;
+    a = 9.8;
+
+    //input mass
+    cout << "Enter a number and press ENTER: ";
+    cin >> m;
+
+    g = m * a;
+    cout << "The Force of gravity is: " << g << "ms^2";
+    return 0;
+}
