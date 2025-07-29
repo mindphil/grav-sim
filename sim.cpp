adf947b38342c51adf2090cf181c33c8ec9083a7
mindphil <philipumeadi@icloud.com>
2025-07-28 23:49:54 -0400

    determining gravity with variable mass inputs.

--- sim.cpp
+++ sim.cpp
@@ -14,6 +14,16 @@ int main()
     cout << "Enter a number and press ENTER: ";
     cin >> m;
 
+    //Error if entry is not a number
+    if (cin.fail()){
+        cout << "Input a number!";
+        return 0;
+    }
+    //Error if entry is less than or equal to zero
+    else if (m <= 0){
+        cout << "Mass cannot be negative or zero!";
+        return 0;
+    }
     g = m * a;
     cout << "The Force of gravity is: " << g << "ms^2";
     return 0;
