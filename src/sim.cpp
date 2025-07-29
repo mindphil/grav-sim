5d141dd0972fa130d5d9398afa091d76e65ad9ff
mindphil <philipumeadi@icloud.com>
2025-07-29 17:20:47 -0400

    cmake struggle

--- src/sim.cpp
+++ src/sim.cpp
@@ -23,3 +23,19 @@ int main()
     std::cout << "The Force of gravity is: " << g << "N\n";
     return 0;
 }
+
+// Newton's law of universal gravitation
+
+int ug()
+{
+    float G =  6.67e-11;  // Gravitational constant in Nm2/kg^2
+    float F = 0.0f, m1 = 0.0f, m2 = 0.0f, r = 0.0f;
+    F = (G * ((m1 * m2)/(r * r)));  // F = G((m1 * m2)/r^2)
+
+    // two planets
+    m1 = 5.972e24;
+    m2 = 7.35e22;
+    r = 385,000/2; //distance between them/2
+    std::cout << F;
+    return 0;
+}
