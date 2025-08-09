ef59194667ba3d5d78231e609f02faabb99457c6
mindphil <philipumeadi@icloud.com>
2025-08-09 15:12:00 -0400

    cleaned up inst of c++, using clang now. I got a better undterstand of complination & build tools.

--- src/sim.cpp
+++ src/sim.cpp
@@ -29,13 +29,14 @@ int main()
 int ug()
 {
     float G =  6.67e-11;  // Gravitational constant in Nm2/kg^2
-    float F = 0.0f, m1 = 0.0f, m2 = 0.0f, r = 0.0f;
+    float F = 0.0f, m1 = 0.0f, m2 = 0.0f, r = 0.0f, d = 0.0f;
+    r = d/2;
     F = (G * ((m1 * m2)/(r * r)));  // F = G((m1 * m2)/r^2)
 
     // two planets
     m1 = 5.972e24;
     m2 = 7.35e22;
-    r = 385,000/2; //distance between them/2
+    d = 385,000;
     std::cout << F;
     return 0;
 }
